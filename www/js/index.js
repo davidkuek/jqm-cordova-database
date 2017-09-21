d2978b21a7da27a7aaffa37d530b026aab91db97
david kuek <davidkuek1990@gmail.com>
2017-09-21 15:48:34 +0800

    first commit

---
+++ www/js/index.js
@@ -0,0 +1,38 @@
+$( document ).on( "mobileinit", function() {
+    
+    $('#add_contact').on("click",addContact)
+    $('#show_contact').on("click",showContact)
+  
+});
+
+var db = openDatabase('mydb', '1.0', 'Test DB', 2 * 1024 * 1024);
+
+function addContact(){
+try{db.transaction(function (tx,results) {
+   tx.executeSql('CREATE TABLE IF NOT EXISTS CARPENTERS (id unique, name)');
+   tx.executeSql('INSERT INTO CARPENTERS (id, name) VALUES (1, "Lee")');
+
+});
+}
+catch(err){
+    console.log(err);
+}
+}
+
+function showContact(){
+db.transaction(function (tx) {
+   tx.executeSql('SELECT * FROM CARPENTERS', [], function (tx, results) {
+      var len = results.rows.length, i;
+      // msg = "<p>Found rows: " + len + "</p>";
+      // document.querySelector('#status').innerHTML += msg ;
+    
+      for (i = 0; i < len; i++){
+         // alert(results.rows.item(i).name );
+         document.querySelector('#status').innerHTML += results.rows.item(i).name ;
+         
+      }
+    
+   }, null);
+});
+}
+
